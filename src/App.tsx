f84f056a463b9cb4e53cbcab29ea4b444540b65e
Issam Eleyan <issam@framestudio.tv>
2026-04-27 11:31:14 +0200

    Remove University Services section

--- src/App.tsx
+++ src/App.tsx
@@ -400,15 +400,6 @@ function App() {
         </motion.div>
       </motion.section>
 
-      {/* University Services */}
-      <motion.div
-        initial={{ opacity: 0, y: 30 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, amount: 0.2 }}
-        transition={{ duration: 0.8 }}
-      >
-        <UniversityServices />
-      </motion.div>
 
       {/* Recognition Section (Why Huna Lina) */}
       <motion.div
